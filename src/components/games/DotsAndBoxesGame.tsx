95389f2d50cb4823b0e834ca0b8713c067774cd0
Brennan Shea <bshea@iMac.local>
2025-12-17 01:44:57 -0500

    fix(mobile): center Dots and Boxes game board vertically

--- src/components/games/DotsAndBoxesGame.tsx
+++ src/components/games/DotsAndBoxesGame.tsx
@@ -64,7 +64,7 @@ export default function DotsAndBoxesGame({
     };
 
     return (
-        <div className="flex flex-col items-center justify-center h-full w-full max-w-2xl md:max-w-4xl mx-auto p-2 sm:p-4 select-none">
+        <div className="flex-1 flex flex-col items-center justify-center h-full w-full max-w-2xl md:max-w-4xl mx-auto p-2 sm:p-4 select-none">
             {/* Prominent Turn Indicator */}
 
 
